10ec9c034232750aceda4ed3cc987c34b6489291
マミタロス <rockman_item2gou_630@icloud.com>
2026-07-06 20:30:41 +0900

    Update artworks.js

--- artworks.js
+++ artworks.js
@@ -214,5 +214,14 @@ const artworks = [
     tags: ["ドラクエ", "一枚絵"],
     comment: "シド―きゅんしゅき",
     draft: false
+  },
+  {
+    title: "エルシオンラピス",
+    date: "2023-03-17",
+    thumb: "images/thumbnails/2023/03/20230317_dorakue_rapisu_th.webp",
+    image: "images/originals/2023/03/20230317_dorakue_rapisu.webp",
+    tags: ["ドラクエ", "一枚絵"],
+    comment: "学園繋がり",
+    draft: false
   }
 ];

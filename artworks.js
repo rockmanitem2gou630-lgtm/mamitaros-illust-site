500c9055c8046eed62353f565625ca33a2b0b651
マミタロス <rockman_item2gou_630@icloud.com>
2026-07-06 20:10:47 +0900

    Update artworks.js

--- artworks.js
+++ artworks.js
@@ -205,5 +205,23 @@ const artworks = [
     tags: ["E7", "一枚絵"],
     comment: "今でもうちじゃ戦闘力1位",
     draft: false
+  },
+  {
+    title: "シド主",
+    date: "2023-03-10",
+    thumb: "images/thumbnails/2023/03/20230310_dorakue_sidoshu_th.webp",
+    image: "images/originals/2023/03/20230310_dorakue_sidoshu.webp",
+    tags: ["ドラクエ", "一枚絵"],
+    comment: "シドーきゅんしゅき",
+    draft: false
+  },
+  {
+    title: "エルシオンラピス",
+    date: "2023-03-17",
+    thumb: "images/thumbnails/2023/03/20230317_dorakue_rapisu_th.webp",
+    image: "images/originals/2023/03/20230317_dorakue_rapisu.webp",
+    tags: ["E7", "一枚絵"],
+    comment: "学園繋がり",
+    draft: false
   }
 ];

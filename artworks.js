dd6b7973ec99bc27e51682880b8fd7bfa28de365
マミタロス <rockman_item2gou_630@icloud.com>
2026-07-06 14:28:26 +0900

    Create artworks.js

---
+++ artworks.js
@@ -0,0 +1,20 @@
+const artworks = [
+  {
+    title: "サンプル作品 1",
+    date: "2026-07-06",
+    image: "https://placehold.co/600x800?text=Art+1",
+    tags: ["電王", "イラスト"]
+  },
+  {
+    title: "サンプル作品 2",
+    date: "2026-06-28",
+    image: "https://placehold.co/600x600?text=Art+2",
+    tags: ["刀剣乱舞", "SD"]
+  },
+  {
+    title: "サンプル作品 3",
+    date: "2026-05-10",
+    image: "https://placehold.co/600x750?text=Art+3",
+    tags: ["一次創作", "カラー"]
+  }
+];
